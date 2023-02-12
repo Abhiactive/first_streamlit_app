f8b6893f336539f5744081d3c349a91c1106a2c1
Abhiactive <124774102+Abhiactive@users.noreply.github.com>
2023-02-12 23:20:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,7 @@ def get_fruit_load_list():
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
+    
 
   
 streamlit.stop()
